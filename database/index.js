1525c10591c745939bfb1d178f723b33fa882f7e
muhammadafifpratama <38064482+muhammadafifpratama@users.noreply.github.com>
2021-02-20 15:45:58 +0700

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,12 +1,6 @@
-let {MongoClient, ObjectID, url} = require('./mongodb')
 let db = require("./mysql")
 
 module.exports = {
-    mongodb: {
-        MongoClient,
-        ObjectID, 
-        url
-    },
     mysql: {
         db
     }
