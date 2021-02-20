955c9f8d4d73d17dac7d2f3619c7af3d30a60af2
afifpratama <afifpratama@gmail.com>
2021-02-21 02:19:32 +0700

    tambah lagi

--- controller/dashboardcontroller.js
+++ controller/dashboardcontroller.js
@@ -31,4 +31,94 @@ module.exports = {
             res.status(200).send(results)
         });
     },
+    addmateri: (req, res) => {
+        var connection = mysql.db
+        let sql = 'insert into materi set ?'
+        connection.query(sql, req.body, (err, results) => {
+            if (err) {
+                return res.status(500).send(err)
+            }
+            res.status(200).send(results)
+        })
+    },
+    materisaya: (req, res) => {
+        var connection = mysql.db
+        let sql = `select * from materi where iduser='${req.body.id}'`
+        connection.query(sql, (err, results) => {
+            if (err) {
+                return res.status(500).send(err)
+            }
+            res.status(200).send(results)
+        })
+    },
+    deletemateri: (req, res) => {
+        var connection = mysql.db
+        let sql = `delete from materi where idmateri='${req.body.id}'`
+        connection.query(sql, (err, results) => {
+            if (err) {
+                return res.status(500).send(err)
+            }
+            res.status(200).send(results)
+        })
+    },
+    terakhir: (req, res) => {
+        var connection = mysql.db
+        let sql = 'insert into recent set ?'
+        connection.query(sql, req.body, (err, results) => {
+            if (err) {
+                return res.status(500).send(err)
+            }
+            res.status(200).send(results)
+        })
+    },
+    terakhir_dipelajari: (req, res) => {
+        var connection = mysql.db
+        let sql = `select * from recent where iduser='${req.body.id}'`
+        connection.query(sql, req.body, (err, results) => {
+            if (err) {
+                return res.status(500).send(err)
+            }
+            res.status(200).send(results)
+        })
+    },
+    delete_recent: (req, res) => {
+        var connection = mysql.db
+        let sql = `delete from recent where idrecent=${req.body.id}`
+        connection.query(sql, req.body, (err, results) => {
+            if (err) {
+                return res.status(500).send(err)
+            }
+            res.status(200).send(results)
+        })
+    },
+    paket: (req, res) => {
+        var connection = mysql.db
+        let sql = 'insert into paket set ?'
+        connection.query(sql, req.body, (err, results) => {
+            if (err) {
+                return res.status(500).send(err)
+            }
+            res.status(200).send(results)
+        })
+    },
+    liatpaket: (req, res) => {
+        var connection = mysql.db
+        let sql = `select * from paket where iduser='${req.body.id}'`
+        connection.query(sql, req.body, (err, results) => {
+            if (err) {
+                return res.status(500).send(err)
+            }
+            res.status(200).send(results)
+        })
+    },
+    deletepaket: (req, res) => {
+        var connection = mysql.db
+        let sql = `delete from paket where idpaket=${req.body.id}`
+        connection.query(sql, req.body, (err, results) => {
+            if (err) {
+                return res.status(500).send(err)
+            }
+            res.status(200).send(results)
+        })
+    },
 }
--- router/dashboardrouter.js
+++ router/dashboardrouter.js
@@ -5,5 +5,14 @@ const router = express.Router();
 router.patch('/wallet', dashboard.isiwallet)
 router.patch('/diamond', dashboard.isidiamond)
 router.get('/', dashboard.dashboardview)
+router.post("/addmateri", dashboard.addmateri)
+router.get("/materi", dashboard.materisaya)
+router.delete("/hapusmateri", dashboard.deletemateri)
+router.post("/addrecent", dashboard.terakhir)
+router.get("/recent", dashboard.terakhir_dipelajari)
+router.delete("/deleterecent", dashboard.delete_recent)
+router.post("/paket", dashboard.paket)
+router.get("/paketsaya", dashboard.liatpaket)
+router.delete("/deletepaket", dashboard.deletepaket)
 
 module.exports = router
